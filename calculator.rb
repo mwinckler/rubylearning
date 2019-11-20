215daddb3cb2969e63948b2b5f7a8257d5b8aa75
Hans Winckler <56323358+Hansicrater@users.noreply.github.com>
2019-11-19 19:25:49 -0800

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,3 @@
-# The exponent operator in ruby is **
-
 def add(operand_1, operand_2)
   return operand_1 + operand_2
 end
@@ -25,7 +23,6 @@ def root(operand_1)
 end
 
 # ask the user what kind of math operation he wants to do
-# (add, subtract, multiply, divide)
 puts "Give me an operation!"
 operator = gets.chomp
 
@@ -55,22 +52,5 @@ when "sqrt"
 else
   puts "I have no idea what you intended to do here."
 end
-
-# Instead of a case statement, we could use basic conditionals.
-# The following lines do the same thing as the case statement above:
-#
-#if operator == "+"
- #  result = add(operand_1, operand_2)
- #elsif operator == "-"
-  # result = subtract(operand_1, operand_2)
- #elsif operator == "*"
- #  result = multiply(operand_1, operand_2)
- #elsif operator == "/"
- #  result = divide(operand_1, operand_2)
- #elsif operator == "^"
- #   result = exponent(operand_1, operand_2)
- #else
- #  puts "I have no idea what you intended to do here."
-#end
 
 print result
