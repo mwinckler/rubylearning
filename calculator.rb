53a101c54fa75fd7635e0a8236d00e61f834b384
Hans Winckler <me@hanswinckler.com>
2019-11-26 19:29:20 -0800

    Deleted uneccessary commenting out of useless code

--- calculator.rb
+++ calculator.rb
@@ -56,21 +56,4 @@ else
   puts "I have no idea what you intended to do here."
 end
 
-# Instead of a case statement, we could use basic conditionals.
-# The following lines do the same thing as the case statement above:
-#
-#if operator == "+"
- #  result = add(operand_1, operand_2)
- #elsif operator == "-"
-  # result = subtract(operand_1, operand_2)
- #elsif operator == "*"
- #  result = multiply(operand_1, operand_2)
- #elsif operator == "/"
- #  result = divide(operand_1, operand_2)
- #elsif operator == "^"
- #   result = exponent(operand_1, operand_2)
- #else
- #  puts "I have no idea what you intended to do here."
-#end
-
 print result
